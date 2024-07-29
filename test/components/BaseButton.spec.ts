433c298e2e8246a7490fd9d3b7866e22616294db
Igor Babko <i.i.babko@gmail.com>
2024-07-29 11:32:23 +0300

    02. BaseButton component test

---
+++ test/components/BaseButton.spec.ts
@@ -0,0 +1,17 @@
+// @vitest-environment happy-dom
+
+import { mount } from '@vue/test-utils'
+import { expect, it } from 'vitest'
+import BaseButton from '../../src/components/BaseButton.vue'
+
+it('renders primary button by default', () => {
+  const label = 'Label'
+  const primaryButtonClasses = 'bg-purple-500 enabled:hover:bg-purple-600 text-white'
+
+  const wrapper = mount(BaseButton, {
+    slots: { default: label }
+  })
+
+  expect(wrapper.html()).toContain(label)
+  expect(wrapper.classes()).toEqual(expect.arrayContaining(primaryButtonClasses.split(' ')))
+})
