ebf1d105b7a896d19e337780b76913559f4fe14e
Igor Babko <i.i.babko@gmail.com>
2024-08-07 09:49:38 +0300

    ActivityItem

--- test/components/ActivityItem.test.ts
+++ test/components/ActivityItem.test.ts
@@ -2,8 +2,13 @@ import { mount } from '@vue/test-utils'
 import { expect, it, vi } from 'vitest'
 import * as activities from '../../src/activities'
 import ActivityItem from '../../src/components/ActivityItem.vue'
-import { SECONDS_IN_HOUR, SECONDS_IN_MINUTE } from '../../src/constants'
+import BaseButton from '../../src/components/BaseButton.vue'
+import BaseIcon from '../../src/components/BaseIcon.vue'
+import BaseSelect from '../../src/components/BaseSelect.vue'
+import RemainingActivitySeconds from '../../src/components/RemainingActivitySeconds.vue'
+import { PERIOD_SELECT_OPTIONS, SECONDS_IN_HOUR, SECONDS_IN_MINUTE } from '../../src/constants'
 import * as timelineItems from '../../src/timeline-items'
+import { ButtonType, IconName } from '../../src/types'
 
 const activity = {
   id: '1',
@@ -11,7 +16,7 @@ const activity = {
   secondsToComplete: SECONDS_IN_HOUR * 1
 }
 
-function mountActivityItem(secondsToComplete: number) {
+function mountActivityItem(secondsToComplete = 0) {
   return mount(ActivityItem, {
     props: {
       activity: { ...activity, secondsToComplete }
@@ -19,53 +24,18 @@ function mountActivityItem(secondsToComplete: number) {
   })
 }
 
-it('shows activity info', () => {
-  const wrapper = mountActivityItem(SECONDS_IN_HOUR * 1).html()
+it('has delete button', () => {
+  const wrapper = mountActivityItem()
 
-  expect(wrapper).toContain('Reading')
-  expect(wrapper).toContain('-01:00:00')
-  expect(wrapper).toContain('01:00')
+  expect(wrapper.findComponent(BaseButton).vm.type).toBe(ButtonType.DANGER)
+  expect(wrapper.findComponent(BaseIcon).vm.name).toBe(IconName.TRASH)
 })
 
-it('does not show seconds to complete if it is 0', () => {
-  expect(mountActivityItem(0).html()).not.toContain('00:00')
-})
-
-it('updates seconds to complete', async () => {
-  const updateActivity = vi.spyOn(activities, 'updateActivity')
-  const wrapper = mountActivityItem(SECONDS_IN_HOUR * 1)
-  const secondsToComplete = SECONDS_IN_MINUTE * 30
-
-  await wrapper.find('select').setValue(`${secondsToComplete}`)
-
-  expect(wrapper.html()).toContain('-00:30:00')
-  expect(wrapper.html()).toContain('00:30')
-  expect(updateActivity).toBeCalledTimes(1)
-  expect(updateActivity).toBeCalledWith({ ...activity, secondsToComplete }, { secondsToComplete })
-
-  vi.restoreAllMocks()
-})
-
-// it('updates seconds to complete to zero if none is selected', async () => {
-//   const updateActivity = vi.spyOn(activities, 'updateActivity')
-//   const wrapper = mountActivityItem(SECONDS_IN_HOUR * 1)
-//   const secondsToComplete = 0
-
-//   await wrapper.find('select').setValue('')
-
-//   expect(wrapper.html()).toContain('hh:mm')
-//   expect(updateActivity)
-//     .toBeCalledTimes(1)
-//     .toBeCalledWith({ ...activity, secondsToComplete }, { secondsToComplete })
-
-//   vi.restoreAllMocks()
-// })
-
-it('deletes activity', async () => {
+it('deletes activity', () => {
   const resetTimelineItemActivities = vi.spyOn(timelineItems, 'resetTimelineItemActivities')
   const deleteActivity = vi.spyOn(activities, 'deleteActivity')
 
-  await mountActivityItem(SECONDS_IN_HOUR * 1)
+  mountActivityItem(SECONDS_IN_HOUR * 1)
     .find('button')
     .trigger('click')
 
@@ -75,4 +45,55 @@ it('deletes activity', async () => {
   expect(deleteActivity).toBeCalledWith(activity)
 
   vi.restoreAllMocks()
+})
+
+it('shows activity name', () => {
+  expect(mountActivityItem().text()).toContain('Reading')
+})
+
+it('has seconds to complete select', () => {
+  const baseSelectWrapper = mountActivityItem(SECONDS_IN_HOUR * 1).findComponent(BaseSelect as any)
+
+  expect(baseSelectWrapper.vm.options).toBe(PERIOD_SELECT_OPTIONS)
+  expect(baseSelectWrapper.vm.selected).toBe(SECONDS_IN_HOUR * 1)
+})
+
+it('updates seconds to complete', async () => {
+  const updateActivity = vi.spyOn(activities, 'updateActivity')
+  const secondsToComplete = SECONDS_IN_MINUTE * 30
+  const wrapper = mountActivityItem(SECONDS_IN_HOUR * 1)
+
+  expect(wrapper.text()).toContain('-01:00:00')
+
+  // await wrapper.find('select').setValue(`${secondsToComplete}`)
+  await wrapper.findComponent(BaseSelect as any).vm.$emit('select', secondsToComplete)
+
+  expect(wrapper.text()).toContain('-00:30:00')
+  expect(updateActivity).toBeCalledTimes(1)
+  expect(updateActivity).toBeCalledWith({ ...activity, secondsToComplete }, { secondsToComplete })
+
+  vi.restoreAllMocks()
+})
+
+it('updates seconds to complete to zero if none is selected', () => {
+  const updateActivity = vi.spyOn(activities, 'updateActivity')
+  const wrapper = mountActivityItem(SECONDS_IN_HOUR * 1)
+  const secondsToComplete = 0
+
+  wrapper.findComponent(BaseSelect as any).vm.$emit('select')
+
+  expect(updateActivity).toBeCalledTimes(1)
+  expect(updateActivity).toBeCalledWith({ ...activity, secondsToComplete }, { secondsToComplete })
+
+  vi.restoreAllMocks()
+})
+
+it('has remaining activity seconds', () => {
+  expect(
+    mountActivityItem(SECONDS_IN_HOUR * 1).findComponent(RemainingActivitySeconds).vm.activity
+  ).toEqual(activity)
+})
+
+it('does not have remaining activity seconds if seconds to complete is 0', () => {
+  expect(mountActivityItem(0).findComponent(RemainingActivitySeconds).exists()).toBe(false)
 })
