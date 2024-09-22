ec559eb9cb978ac168f3540812b9dcfba3cff0d8
Igor Babko <i.i.babko@gmail.com>
2024-09-22 19:31:05 +0300

    RemainingActivitySeconds

--- test/components/RemainingActivitySeconds.test.ts
+++ test/components/RemainingActivitySeconds.test.ts
@@ -1,54 +1,58 @@
 import { shallowMount } from '@vue/test-utils'
-import { beforeEach, expect, it, vi } from 'vitest'
+import { afterEach, expect, it, vi } from 'vitest'
 import RemainingActivitySeconds from '../../src/components/RemainingActivitySeconds.vue'
 import { SECONDS_IN_HOUR } from '../../src/constants'
 import { formatSecondsWithSign } from '../../src/functions'
 import * as timelineItems from '../../src/timeline-items'
+import { Activity } from '../../src/types'
 
-const activity = {
-  id: '1',
-  name: 'Reading',
-  secondsToComplete: SECONDS_IN_HOUR * 1
-}
-
-function shallowMountRemainingActivitySeconds() {
+function shallowMountRemainingActivitySeconds(activityOverrides: Partial<Activity> = {}) {
   return shallowMount(RemainingActivitySeconds, {
-    props: { activity }
+    props: {
+      activity: {
+        id: '',
+        name: '',
+        secondsToComplete: 0,
+        ...activityOverrides
+      }
+    }
   })
 }
 
-beforeEach(() => {
+afterEach(() => {
   vi.restoreAllMocks()
 })
 
 it('shows remaining activity seconds', () => {
+  const secondsToComplete = SECONDS_IN_HOUR * 1
   const trackedActivitySeconds = SECONDS_IN_HOUR * 2
-
   vi.spyOn(timelineItems, 'calculateTrackedActivitySeconds').mockReturnValue(trackedActivitySeconds)
 
-  expect(shallowMountRemainingActivitySeconds().text()).toContain(
-    formatSecondsWithSign(trackedActivitySeconds - activity.secondsToComplete)
+  const wrapper = shallowMountRemainingActivitySeconds({ secondsToComplete })
+
+  expect(wrapper.text()).toContain(
+    formatSecondsWithSign(trackedActivitySeconds - secondsToComplete)
   )
 })
 
 it('has positive state classes if tracked activity seconds are >= activity seconds to complete', () => {
   const classes = 'bg-green-100 text-green-600'
-  const trackedActivitySeconds = SECONDS_IN_HOUR * 2
-
+  const secondsToComplete = SECONDS_IN_HOUR * 1
+  const trackedActivitySeconds = secondsToComplete * 2
   vi.spyOn(timelineItems, 'calculateTrackedActivitySeconds').mockReturnValue(trackedActivitySeconds)
 
-  expect(shallowMountRemainingActivitySeconds().classes()).toEqual(
-    expect.arrayContaining(classes.split(' '))
-  )
+  const wrapper = shallowMountRemainingActivitySeconds({ secondsToComplete })
+
+  expect(wrapper.classes()).toEqual(expect.arrayContaining(classes.split(' ')))
 })
 
 it('has negative state classes if tracked activity seconds are < activity seconds to complete', () => {
   const classes = 'bg-red-100 text-red-600'
-  const trackedActivitySeconds = SECONDS_IN_HOUR * 0.5
-
+  const secondsToComplete = SECONDS_IN_HOUR * 1
+  const trackedActivitySeconds = secondsToComplete / 2
   vi.spyOn(timelineItems, 'calculateTrackedActivitySeconds').mockReturnValue(trackedActivitySeconds)
 
-  expect(shallowMountRemainingActivitySeconds().classes()).toEqual(
-    expect.arrayContaining(classes.split(' '))
-  )
+  const wrapper = shallowMountRemainingActivitySeconds({ secondsToComplete })
+
+  expect(wrapper.classes()).toEqual(expect.arrayContaining(classes.split(' ')))
 })
