318e49a11fa813703ff43ae221303b7939c4e9cd
Igor Babko <i.i.babko@gmail.com>
2024-09-22 12:23:34 +0300

    progressitem

--- test/components/ProgressItem.test.ts
+++ test/components/ProgressItem.test.ts
@@ -1,55 +1,68 @@
 import { shallowMount } from '@vue/test-utils'
-import { afterAll, expect, it, vi } from 'vitest'
+import { afterEach, expect, it, vi } from 'vitest'
 import { computed } from 'vue'
 import ProgressItem from '../../src/components/ProgressItem.vue'
 import { useProgress } from '../../src/composables/progress'
-import { HUNDRED_PERCENT, MEDIUM_PERCENT, SECONDS_IN_HOUR } from '../../src/constants'
+import { HUNDRED_PERCENT, LOW_PERCENT, MEDIUM_PERCENT, SECONDS_IN_HOUR } from '../../src/constants'
 import { formatSeconds } from '../../src/functions'
-import { ProgressColorClass } from '../../src/types'
+import { Activity, ProgressColorClass } from '../../src/types'
 
-vi.mock('../../src/composables/progress', () => {
-  return {
-    useProgress: vi.fn(() => ({
-      percentage: computed(() => MEDIUM_PERCENT),
-      colorClass: computed(() => ProgressColorClass.BLUE),
-      trackedActivitySeconds: computed(() => SECONDS_IN_HOUR * 1)
-    }))
-  }
-})
+vi.mock('../../src/composables/progress')
 
-const activity = {
-  id: '1',
-  name: 'Reading',
-  secondsToComplete: SECONDS_IN_HOUR * 1
-}
-
-function shallowMountProgressItem() {
+function shallowMountProgressItem(activityOverrides: Partial<Activity> = {}) {
   return shallowMount(ProgressItem, {
-    props: { activity }
+    props: {
+      activity: {
+        id: '',
+        name: '',
+        secondsToComplete: 0,
+        ...activityOverrides
+      }
+    }
   })
 }
 
-afterAll(() => {
+function mockUseProgressComposable(overrides: Partial<ReturnType<typeof useProgress>> = {}) {
+  vi.mocked(useProgress).mockReturnValue({
+    colorClass: computed(() => ProgressColorClass.GREEN),
+    percentage: computed(() => HUNDRED_PERCENT),
+    trackedActivitySeconds: computed(vi.fn()),
+    ...overrides
+  })
+}
+
+afterEach(() => {
   vi.restoreAllMocks()
 })
 
 it('shows activity name', () => {
-  expect(shallowMountProgressItem().text()).toContain(activity.name)
+  mockUseProgressComposable()
+  const name = 'Reading'
+
+  expect(shallowMountProgressItem({ name }).text()).toContain(name)
 })
 
 it('shows activity completion percentage', () => {
-  expect(shallowMountProgressItem().text()).toContain(MEDIUM_PERCENT)
+  mockUseProgressComposable({ percentage: computed(() => LOW_PERCENT) })
+
+  expect(shallowMountProgressItem().text()).toContain(LOW_PERCENT)
 })
 
 it('shows tracked activity seconds and activity seconds to complete', () => {
-  expect(shallowMountProgressItem().text()).toContain(
-    `${formatSeconds(SECONDS_IN_HOUR * 1)} / ${formatSeconds(activity.secondsToComplete)}`
+  const secondsToComplete = SECONDS_IN_HOUR * 1
+  const trackedActivitySeconds = secondsToComplete / 2
+  mockUseProgressComposable({ trackedActivitySeconds: computed(() => trackedActivitySeconds) })
+
+  expect(shallowMountProgressItem({ secondsToComplete }).text()).toContain(
+    `${formatSeconds(trackedActivitySeconds)} / ${formatSeconds(secondsToComplete)}`
   )
 })
 
 it('shows incomplete progress bar if percentage is < 100%', () => {
-  // expect(wrapper.html()).toContain(ProgressColorClass.BLUE)
-  // expect(wrapper.html()).toContain(`width: ${Math.min(MEDIUM_PERCENT, HUNDRED_PERCENT)}%`)
+  mockUseProgressComposable({
+    colorClass: computed(() => ProgressColorClass.BLUE),
+    percentage: computed(() => MEDIUM_PERCENT)
+  })
 
   const progressBarElement = shallowMountProgressItem().find(`.${ProgressColorClass.BLUE}`)
     .element as HTMLElement
@@ -57,27 +70,25 @@ it('shows incomplete progress bar if percentage is < 100%', () => {
   expect(progressBarElement.style.width).toContain(`${MEDIUM_PERCENT}%`)
 })
 
-it('shows complete progress bar if percentage is 100%', () => {
-  vi.mocked(useProgress).mockReturnValue({
-    percentage: computed(() => HUNDRED_PERCENT),
-    colorClass: computed(() => ProgressColorClass.BLUE),
-    trackedActivitySeconds: computed(vi.fn())
+it('shows complete progress bar if percentage is = 100%', () => {
+  mockUseProgressComposable({
+    colorClass: computed(() => ProgressColorClass.GREEN),
+    percentage: computed(() => HUNDRED_PERCENT)
   })
 
-  const progressBarElement = shallowMountProgressItem().find(`.${ProgressColorClass.BLUE}`)
+  const progressBarElement = shallowMountProgressItem().find(`.${ProgressColorClass.GREEN}`)
     .element as HTMLElement
 
   expect(progressBarElement.style.width).toContain(`${HUNDRED_PERCENT}%`)
 })
 
 it('shows complete progress bar if percentage is > 100%', () => {
-  vi.mocked(useProgress).mockReturnValue({
-    percentage: computed(() => HUNDRED_PERCENT + 1),
-    colorClass: computed(() => ProgressColorClass.BLUE),
-    trackedActivitySeconds: computed(vi.fn())
+  mockUseProgressComposable({
+    colorClass: computed(() => ProgressColorClass.GREEN),
+    percentage: computed(() => HUNDRED_PERCENT + 1)
   })
 
-  const progressBarElement = shallowMountProgressItem().find(`.${ProgressColorClass.BLUE}`)
+  const progressBarElement = shallowMountProgressItem().find(`.${ProgressColorClass.GREEN}`)
     .element as HTMLElement
 
   expect(progressBarElement.style.width).toContain(`${HUNDRED_PERCENT}%`)
