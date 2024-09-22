b8ecf08ed5adf9496ab618064fb472252e984961
Igor Babko <i.i.babko@gmail.com>
2024-09-22 21:18:49 +0300

    timelinehour

--- test/components/TimelineHour.test.ts
+++ test/components/TimelineHour.test.ts
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
-import { expect, it, vi } from 'vitest'
+import { afterAll, describe, expect, it, vi } from 'vitest'
 import TimelineHour from '../../src/components/TimelineHour.vue'
+import * as timelineItems from '../../src/timeline-items'
 import { Hour } from '../../src/types'
 
 function shallowMountTimelineHour(hour: Hour) {
@@ -13,22 +14,38 @@ it.each([0, 5, 10, 23] as Hour[])('shows formatted hour %i', (hour) => {
   expect(shallowMountTimelineHour(hour).text()).toContain(`${hour.toString().padStart(2, '0')}:00`)
 })
 
-it('has active state classes if timeline hour is current hour', () => {
-  const classes = 'bg-purple-900 font-black text-white'
+it('scrolls to particular hour on click', () => {
+  const scrollToHourSpy = vi.spyOn(timelineItems, 'scrollToHour')
+  const hour = 8
 
-  vi.hoisted(() => vi.setSystemTime(new Date(`2024-04-10 08:00:00`)))
+  shallowMountTimelineHour(hour).trigger('click')
 
-  expect(shallowMountTimelineHour(8).classes()).toEqual(expect.arrayContaining(classes.split(' ')))
+  expect(scrollToHourSpy).toBeCalledTimes(1)
+  expect(scrollToHourSpy).toBeCalledWith(hour)
 
-  vi.useRealTimers()
+  vi.restoreAllMocks()
 })
 
-it('has normal state classes if timeline hour is not current hour', () => {
-  const classes = 'bg-gray-100 text-gray-500'
-
+describe('classes', () => {
   vi.hoisted(() => vi.setSystemTime(new Date(`2024-04-10 08:00:00`)))
 
-  expect(shallowMountTimelineHour(9).classes()).toEqual(expect.arrayContaining(classes.split(' ')))
+  const currentHour = 8
 
-  vi.useRealTimers()
+  it('has active state classes if timeline hour is current', () => {
+    const classes = 'bg-purple-900 font-black text-white'
+    const wrapper = shallowMountTimelineHour(currentHour)
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(classes.split(' ')))
+  })
+
+  it('has normal state classes if timeline hour is not current', () => {
+    const classes = 'bg-gray-100 text-gray-500'
+    const wrapper = shallowMountTimelineHour((currentHour + 1) as Hour)
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(classes.split(' ')))
+  })
+
+  afterAll(() => {
+    vi.useRealTimers()
+  })
 })
