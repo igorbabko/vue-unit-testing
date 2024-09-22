3b5afcd9be7c8dbcd69130d1d4b8ed88c6c0f318
Igor Babko <i.i.babko@gmail.com>
2024-09-22 21:51:51 +0300

    timelineitem

--- test/components/TimelineItem.test.ts
+++ test/components/TimelineItem.test.ts
@@ -4,26 +4,26 @@ import BaseSelect from '../../src/components/BaseSelect.vue'
 import TimelineHour from '../../src/components/TimelineHour.vue'
 import TimelineItem from '../../src/components/TimelineItem.vue'
 import TimelineStopwatch from '../../src/components/TimelineStopwatch.vue'
-import { SECONDS_IN_HOUR } from '../../src/constants'
 import * as timelineItems from '../../src/timeline-items'
-import { TimelineItem as TimelineItemType } from '../../src/types'
+import { SelectOption, TimelineItem as TimelineItemType } from '../../src/types'
 
-vi.mock('../../src/activities', () => ({ activitySelectOptions: getActivitySelectOptions() }))
-
-const timelineItem: TimelineItemType = {
-  hour: 8,
-  activityId: '2',
-  activitySeconds: SECONDS_IN_HOUR * 1,
-  isActive: false
-}
-
-function shallowMountTimelineItem() {
+function shallowMountTimelineItem(overrides: Partial<TimelineItemType> = {}) {
   return shallowMount(TimelineItem, {
-    props: { timelineItem }
+    props: { timelineItem: createTimelineItem(overrides) }
   })
 }
 
-function getActivitySelectOptions() {
+function createTimelineItem(overrides: Partial<TimelineItemType> = {}): TimelineItemType {
+  return {
+    hour: 0,
+    activityId: null,
+    activitySeconds: 0,
+    isActive: false,
+    ...overrides
+  }
+}
+
+function getActivitySelectOptions(): SelectOption[] {
   return [
     { value: '1', label: 'Training' },
     { value: '2', label: 'Reading' },
@@ -32,26 +32,32 @@ function getActivitySelectOptions() {
 }
 
 it('renders timeline item', () => {
-  const wrapper = shallowMountTimelineItem()
+  vi.mock('../../src/activities', () => ({ activitySelectOptions: getActivitySelectOptions() }))
+  const hour = 8
+  const activityId = '1'
+  const timelineItem = createTimelineItem({ hour, activityId })
 
-  expect(wrapper.findComponent(TimelineHour).props('hour')).toBe(timelineItem.hour)
-  expect(wrapper.findComponent(BaseSelect as any).props('selected')).toBe(timelineItem.activityId)
-  expect(wrapper.findComponent(BaseSelect as any).props('options')).toEqual(
-    getActivitySelectOptions()
-  )
+  const wrapper = shallowMountTimelineItem(timelineItem)
+
+  expect(wrapper.findComponent(TimelineHour).props('hour')).toBe(hour)
   expect(wrapper.findComponent(TimelineStopwatch).props('timelineItem')).toEqual(timelineItem)
+  expect(wrapper.findComponent(BaseSelect as any).props()).toEqual({
+    placeholder: 'Rest',
+    selected: activityId,
+    options: getActivitySelectOptions()
+  })
 })
 
 it('updates timeline item when selecting activity', () => {
   const updateTimelineItemSpy = vi.spyOn(timelineItems, 'updateTimelineItem')
-  const activityId = 1
+  const activityId = '1'
+  const timelineItem = createTimelineItem({ activityId: null })
 
-  shallowMountTimelineItem()
+  shallowMountTimelineItem(timelineItem)
     .findComponent(BaseSelect as any)
     .vm.$emit('select', activityId)
 
   expect(updateTimelineItemSpy).toBeCalledTimes(1)
-  expect(updateTimelineItemSpy).toBeCalledWith(timelineItem, { activityId })
-
+  expect(updateTimelineItemSpy).toBeCalledWith({ ...timelineItem, activityId }, { activityId })
   vi.restoreAllMocks()
 })
